384f88d7c5e03e8821bc16a64d78538772536ca1
siriushari <siriushari0@gmail.com>
2021-11-24 18:58:55 +0530

    updated review project

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Review from './Review';
 
 function App() {
-  return ( <main>
-   <section className='container'>
-     <div className='title'>
-       <h1>Our Reviews</h1>
-       <div className='underline'></div>
-     </div>
-     <Review/>
-   </section>
-   </main>
-  )
+  return (
+    <main>
+      <section className='container'>
+      <div className='title'>
+        <h1>Our Reviews</h1>
+        <div className='underline'></div>
+      </div>
+      <Review/>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -3,57 +3,67 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  const [index, setIndex] = useState(0)
-  const {name, job, image, text} = people[index]
+ const [index, setIndex] = useState(0)
+ const {name, job, text, image} = people[index]
 
-  const checkNumber = (number)=>{
-if (number > people.length - 1){
-  return 0 
+
+ const checkNumber = (number)=>{
+if(number > people.length - 1){
+  return 0
 }
-if (number < 0){
-  return people.length - 1
+if(number < 0){
+  return people.length - 1 
 }
 return number
-  }
-
-  const prevPerson = ()=>{
-    setIndex((index)=>{
-   let newIndex = index - 1
-   return checkNumber(newIndex)
-    })
-  }
-
-  const nextPerson = ()=>{
-    setIndex((index)=>{
-   let newIndex = index + 1
-   return checkNumber(newIndex)
-    })
-  }
+ }
 
 const randomPerson = ()=>{
-  let randomNumber = Math.floor(Math.random() * people.length)
+  let randomNumber = Math.floor( Math.random() * people.length)
   if(randomNumber === index){
     randomNumber = index + 1
   }
-  return setIndex(checkNumber(randomNumber))
+  setIndex(checkNumber(randomNumber))
 }
 
-  return (
-    <article className='review'>
-      <div className='img-container'>
-        <img src={image} alt={name} className='person-img'></img>
-        <span className='quote-icon'> <FaQuoteRight/></span>
-      </div>
-      <h4 className='author'>{name}</h4>
-        <p className='jon'>{job}</p>
-        <p className='info'>{text}</p>
-        <div className='button-container'>
-        <button className='prev-btn' onClick={prevPerson}><FaChevronLeft/></button>
-        <button className='next-btn' onClick={nextPerson}><FaChevronRight/></button>
-         </div>
-        <button className='random-btn' onClick={randomPerson}>surprise me</button>
-    </article>
-  );
+
+
+const nextPerson = ()=>{
+  setIndex((index)=>{
+   let newIndex = index + 1
+   return checkNumber(newIndex)
+  })
+}
+
+const prevPerson = ()=>{
+  setIndex((index)=>{
+   let newIndex = index - 1
+   return checkNumber(newIndex)
+  })
+}
+
+  return(
+  <article className='review'>
+    <div className='img-container'>
+     <img src={image} alt={name} className='person-img'></img>
+     <span className='quote-icon'><FaQuoteRight/></span>
+    </div>
+    <h4 className='author'>{name}</h4>
+    <p className='job'>{job}</p>
+    <p className='info'>{text}</p>
+    <div className='btn-container'>
+      <button className='prev-btn' onClick={prevPerson}>
+    <FaChevronLeft/>
+      </button>
+      <button className='next-btn' onClick={nextPerson}>
+    <FaChevronRight/>
+      </button>
+      
+    </div>
+    <button className='random-btn' onClick={randomPerson}>
+     surprise me
+      </button>
+  </article>
+  )
 };
 
 export default Review;
